e5f0c9dd9f21474d4b87354b466f45f7aa9fd151
Ernesto Arellano <ernestoarellano@icloud.com>
2020-11-23 00:33:36 -0800

    add chakra gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
         url: `https://robertsresorts.bigrigmedia.com/graphql/`,
       },
     },
+    `@chakra-ui/gatsby-plugin`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
